086cf5fac8d2e5391ff157585b215667d87a6cd4
Brian Kang <bhkangw@gmail.com>
2018-01-22 00:47:14 -0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BurbankComponent } from './burbank/burbank.component';
+import { ChicagoComponent } from './chicago/chicago.component';
+import { DallasComponent } from './dallas/dallas.component';
+import { DcComponent } from './dc/dc.component';
+import { SeattleComponent } from './seattle/seattle.component';
+import { SanjoseComponent } from './sanjose/sanjose.component';
+import { LandingComponent } from './landing/landing.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BurbankComponent,
+    ChicagoComponent,
+    DallasComponent,
+    DcComponent,
+    SeattleComponent,
+    SanjoseComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
